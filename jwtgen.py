2fe50bf57053e33b1f0dce390e97d8c68b10a76a
tonunew1 <tonunew1@gmail.com>
2026-08-24 16:59:20 +0600

    Update jwtgen.py

--- jwtgen.py
+++ jwtgen.py
@@ -168,7 +168,7 @@ def get_single_response():
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB53"
+            'ReleaseVersion': "OB54"
         }
 
         response = requests.post(url, data=bytes.fromhex(edata), headers=headers, verify=False)
